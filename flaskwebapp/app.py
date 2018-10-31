64e72d7faecbe05e9feac396fc87310b2999c9aa
aeball <38788316+aeball@users.noreply.github.com>
2018-10-31 10:22:49 +0000

    Add files via upload

---
+++ flaskwebapp/app.py
@@ -0,0 +1,47 @@
+from flask import Flask, request
+import cntk
+from driver import *
+import time
+import flask
+
+app = Flask(__name__)
+
+
+@app.route('/score', methods = ['POST'])
+def scoreRRS():
+    """ Endpoint for scoring
+    """
+    if request.headers['Content-Type'] != 'application/json':
+        return Response(json.dumps({}), status= 415, mimetype ='application/json')
+    input = request.json['input']
+    start = time.time()
+    # Need to add input later
+    response = run()
+    end = time.time() - start
+    dict = {}
+    dict['result'] = response
+    return json.dumps(dict)
+
+@app.route('/test', methods = ['POST'])
+def image():
+    start = time.time()
+    response = run()
+    end = time.time() - start
+    dict = {}
+    dict['result'] = response
+    return json.dumps(dict)
+
+
+@app.route("/")
+def healthy():
+    return "Healthy"
+
+
+# CNTK Version
+@app.route('/version', methods = ['GET'])
+def version_request():
+    return cntk.__version__
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0') # Ignore, Development server
